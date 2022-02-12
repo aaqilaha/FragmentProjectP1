7fc7e55964659cf7aa3d0bea6bf911eee8e03449
aaqilaha <aaqilahanna@mail.ugm.ac.id>
2022-02-12 22:22:05 +0700

    communication

--- app/src/main/java/com/example/fragmentexample1updated/MainActivity.java
+++ app/src/main/java/com/example/fragmentexample1updated/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.fragmentexample1updated;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SimpleFragment.OnFragmentInteractionListener {
     private Button openButton;
     private Boolean isFragmentDisplayed = false;
+
+    private final String FRAGMENT_STATE ="fragment-state";
+    private int mCurrentChoice = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,12 +33,18 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        if(savedInstanceState != null){
+            isFragmentDisplayed = savedInstanceState.getBoolean(FRAGMENT_STATE);
+            if (isFragmentDisplayed){
+                openButton.setText(R.string.close);
+            }
+        }
     }
     private void openFragment(){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        SimpleFragment simpleFragment = SimpleFragment.newInstance();
+        SimpleFragment simpleFragment = SimpleFragment.newInstance(mCurrentChoice);
         fragmentTransaction.add(R.id.fragment_container, simpleFragment).addToBackStack(null).commit();
 
         isFragmentDisplayed = true;
@@ -47,5 +59,17 @@ public class MainActivity extends AppCompatActivity {
 
         isFragmentDisplayed = false;
         openButton.setText(R.string.open);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(FRAGMENT_STATE,isFragmentDisplayed);
+    }
+    
+    @Override
+    public void onRadioButtonChoiceChecked(int choice){
+        mCurrentChoice = choice;
+        Toast.makeText(this, " choice is " + String.valueOf(choice), Toast.LENGTH_SHORT).show();
     }
 }
